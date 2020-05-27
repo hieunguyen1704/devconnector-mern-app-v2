a40d3cc94d7410ef724ed50a42ce682e9fd9effa
hieupropy1999 <hoanghieupro1999@gmail.com>
2020-05-27 10:44:19 +0700

    fix button name

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -110,7 +110,7 @@ const AddEducation = ({ addEducation, history }) => {
             onChange={onChange}
           />
         </div>
-        <input type="submit" className="btn btn-primary my-1" />
+        <input type="submit" className="btn btn-primary my-1" value="Submit"/>
         <Link className="btn btn-light my-1" to="/dashboard">
           Go Back
         </Link>
